e3dc799fbb0f4d0dc5b991ac208ca2ce2e27a9e5
Parth Joshi <parth.joshi592@gmail.com>
2019-06-05 14:56:44 +0530

    Used GoogleHomePage to access waitUntilVisible method

--- autoTestFramework/autoTest-functional-tests/src/main/java/autoTestFramework/functionalTests/homepage/GoogleHomePageTests.java
+++ autoTestFramework/autoTest-functional-tests/src/main/java/autoTestFramework/functionalTests/homepage/GoogleHomePageTests.java
@@ -13,13 +13,11 @@ public class GoogleHomePageTests extends BaseTest {
     @Test
     public void homePageTests() {
         GoogleHomePage homePage = new GoogleHomePage(getDriver());
-        WebDriverWait wait = new WebDriverWait(driver, 1);
         driver.get("https://google.co.in");
 
-        wait.until(ExpectedConditions.visibilityOf(homePage.searchBox));
+        homePage.waitUntilVisible(homePage.searchBox);
         homePage.searchBox.sendKeys("India" + Keys.ENTER);
         assertEquals("India", homePage.searchBox.getAttribute("value"));
-        wait.until(ExpectedConditions.visibilityOf(homePage.resultStats));
+        homePage.waitUntilVisible(homePage.resultStats);
     }
 }
-
